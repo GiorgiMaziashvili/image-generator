f61ac3de637e145c32173b36faea2a4a73d7635a
Giorgi Maziashvili <giorgim@redberry.ge>
2024-02-25 02:28:56 +0400

    init files

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+    mode:"production",
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                type: "asset",
+            },
+        ],
+    },
+      optimization: {
+        minimizer: [
+          new ImageMinimizerPlugin({
+            minimizer: {
+              implementation: ImageMinimizerPlugin.imageminMinify,
+              options: {
+                plugins: [
+                    ["gifsicle", { interlaced: true }],
+                    ["jpegtran", { progressive: true,quality:[0.6,0.8] }],
+                    ['optipng', { optimizationLevel: 5 }],
+                    ['pngquant', { quality: [0.6, 0.8] }]
+                ],
+              },
+            },
+          }),
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+          patterns: [
+            { from: "./src/assets/images", to: "images" },
+          ],
+        }),
+    ],
+};  
